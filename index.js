8b1281ee3f7135cb449d68765ab8de72dbc36536
Gopi Chand Reddy <gopichandreddy001@gmail.com>
2024-06-15 22:37:07 +0530

    added body parser

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ try {
 }
 
 app.use(cors());
-app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/", authenticationRouter);
